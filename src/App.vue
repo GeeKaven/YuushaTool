8c1b3b403e408f36b25ab474ac9579992f482c69
GeeKaven <xiaokang_show@live.com>
2020-03-23 20:38:04 +0800

    添加UI模版

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <header class="mdui-appbar mdui-appbar-fixed">
+      <div class="mdui-toolbar mdui-color-theme">
+        <span
+          class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white"
+          mdui-drawer="{target: '#main-drawer', swipe: true}"
+        >
+          <i class="mdui-icon material-icons">menu</i>
+        </span>
+        <a href="/" class="mdui-typo-headline mdui-hidden-xs">Yuusha.dev</a>
+        <span class="mdui-typo-title">工具箱</span>
+        <div class="mdui-toolbar-spacer"></div>
+        <router-link
+          to="/about"
+          class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white"
+          mdui-tooltip="{content: '关于'}"
+        >
+          <i class="mdui-icon material-icons">info_outline</i>
+        </router-link>
+      </div>
+    </header>
+    <div class="mdui-drawer" id="main-drawer">
+      <div class="mdui-list" mdui-collapse="{accordion: true}">
+        <div class="mdui-collapse-item mdui-collapse-item-open">
+          <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
+            <i class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-blue">near_me</i>
+            <div class="mdui-list-item-content">开始使用</div>
+            <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+          </div>
+          <div class="mdui-collapse-item-body mdui-list" style="height: auto;">
+            <router-link to="/" class="mdui-list-item mdui-ripple">安装</router-link>
+            <router-link to="/" class="mdui-list-item mdui-ripple">兼容性</router-link>
+            <router-link to="/" class="mdui-list-item mdui-ripple">JavaScript 工具库</router-link>
+            <router-link to="/" class="mdui-list-item mdui-ripple">JavaScript 全局方法</router-link>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
